cd5bebf80d137cdae8f92ae127e09d79ae13430f
Nikola-Tesla <foraman202514@gmail.com>
2026-04-18 00:00:17 +0530

    added linearSearch program

--- 06_SingleDimensionalArray/src/_06SearchingArray/_01linearSearchApproach.java
+++ 06_SingleDimensionalArray/src/_06SearchingArray/_01linearSearchApproach.java
@@ -1,5 +1,26 @@
 package _06SearchingArray;
 
 public class _01linearSearchApproach {
-    
+    /*
+    linear search approach compares the key element squentially with each element in the arrays.
+    It continues to do so until the element is found or the array is exhausted without a match being found.
+    If match is made it returns an index of the element.
+    */
+
+    // method for finding the a key in a list
+    public static int linearSearch(int[] list, int key){
+        for(int i = 0; i < list.length; i++){
+            if(key == list[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {1,2,4,6,8,10,12,24};
+        int ans = linearSearch(list, 10);
+        System.out.println(ans); // output: 5
+
+    }
 }
