12803073611c8863051d24adc64ef150ab43963b
Nikola-Tesla <foraman202514@gmail.com>
2026-04-26 00:12:41 +0530

    Added Accessing Object's Data and Methods

--- 08_ObjectsAndClass/src/_02AccessingObjectsviaReferenceVariable/_02accessingObjectsDataAndMethod.java
+++ 08_ObjectsAndClass/src/_02AccessingObjectsviaReferenceVariable/_02accessingObjectsDataAndMethod.java
@@ -15,14 +15,21 @@ public class _02accessingObjectsDataAndMethod {
         circle01.radius = 10;
 
         // Accessing Method
-        circle01.area(4); // output: 
+        circle01.area(4); // output: 6.283185307179586 
     }
 }
 
 class circle{
     int radius;
 
+    /*
+    area() is Instance method thus non-static, it must be invoked by Object reference Variable.
+    however, Math.sqrt is a Static methods defined using static keywords.
+        so if we use Math.sqrt(argument) it will work ✔
+        but only area(argument), will not work you have to invoke reference variable.
+    
+    */
     void area(int radius){
-        System.out.println(Math.PI * Math.sqrt(radius)); // output: 6.283185307179586
+        System.out.println(Math.PI * Math.sqrt(radius));
     }
 }
