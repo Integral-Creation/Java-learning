3e044866ac0c5a70b6a59f905373cae8e7dcd2d6
Nikola-Tesla <foraman202514@gmail.com>
2026-04-25 23:51:26 +0530

    Added constructor intro

---
+++ 08_ObjectsAndClass/src/_01Object&ClassIntro/_04constructorIntro.java
@@ -0,0 +1,11 @@
+
+public class _04constructorIntro {
+    /*
+    Constructor
+        A constructor in java is invoked to create an object using the new operator.
+        Constructor are a special kind of method. They have three peculiarities:
+            • A constructor must have the same name as the class itseflf;
+            • Constructor do not have a return type- not even void;
+            • Constructor are invoked using the new operator when an object is created. Constructor play the role of initializing objects.
+    */
+}
