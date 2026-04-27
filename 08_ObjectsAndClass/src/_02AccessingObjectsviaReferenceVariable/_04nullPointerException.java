017e29157bbe2423887fe204dfd205d7dc4d7607
Nikola-Tesla <foraman202514@gmail.com>
2026-04-27 23:58:55 +0530

    Added NullPointerException program

---
+++ 08_ObjectsAndClass/src/_02AccessingObjectsviaReferenceVariable/_04nullPointerException.java
@@ -0,0 +1,12 @@
+public class _04nullPointerException {
+    public static void main(String[] args) {
+        /*
+        NullPointException in java is happpened when we try ot use a reference variable that is null.
+        */
+
+        /* 
+        String str = null;
+        System.out.println(str.length()); // Exception in thread "main" java.lang.NullPointerException: Cannot invoke "String.length()" because "str" is null
+        */
+    }
+}
