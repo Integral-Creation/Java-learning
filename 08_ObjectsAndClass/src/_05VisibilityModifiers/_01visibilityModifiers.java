340e8c48d247b46deec5f4e904cbc08b7d5d5eaa
Aman <foraman202514@gmail.com>
2026-04-28 23:53:25 +0530

    Added visibility Modifiers

---
+++ 08_ObjectsAndClass/src/_05VisibilityModifiers/_01visibilityModifiers.java
@@ -0,0 +1,23 @@
+package _05VisibilityModifiers;
+
+public class _01visibilityModifiers {
+    /*
+    Visibility Modifiers
+        Visibility Modifiers can be used to specify the Visibility of a class and its members.
+
+        • public:
+            Public Visibility Modifiers for classes, methods, and data fields to denote that they
+            can be accessed from any other classes.
+        • private:
+            Private Modifiers makes methods and data fields accessible only from within its own class.
+        • Default:
+            If no Visibility Modifiers is used, then by default the classes, methods, and data fields
+            accessible by any class in the same package. this is known as package-private or package-access.
+
+        • package:
+            package are used to organize classes
+                Syntax:
+                    package packageName;
+
+    */
+}
