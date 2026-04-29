95dbad16cff3649cda44b9b1abd7117a499f7140
Aman <foraman202514@gmail.com>
2026-04-29 23:31:56 +0530

    Added passing objects to method file

---
+++ 08_ObjectsAndClass/src/_07PassingObjectsToMethod/_01passingObjectToMethod.java
@@ -0,0 +1,10 @@
+package _07PassingObjectsToMethod;
+
+/*
+Passing an object to a method is to pass the reference of the object,
+means the method receives a copy of the reference (memory address), not the object itself.
+*/
+
+public class _01passingObjectToMethod {
+    
+}
