2b9fb7d3d5e2d3e9d8291b79fb64a163227b7f4d
Aman <foraman202514@gmail.com>
2026-05-05 00:02:11 +0530

    Added Character Class

--- 09_StringClass/src/_03CharacterClass/_01characterClass.java
+++ 09_StringClass/src/_03CharacterClass/_01characterClass.java
@@ -3,11 +3,11 @@ package _03CharacterClass;
 The Character class in java, available in java.lang.package is a wrapper class used to represent a single char value as an object
 To enable the primitive data values to be treated as objects, Java provides a class for every primitive data type.
 These classes are Character, Boolean, Byte, Short, Integer, Long, Float, and Double for char, boolean, byte, short, int, long, float, and double, respectively.
-Thses classes are called wrapper classes because each wraps of encapsulates a primitive type value in an object.
+These classes are called wrapper classes because each wraps of encapsulates a primitive type value in an object.
 
 The Character class has a constructor and methods for determining a Character's category and converting Character from uppercase of lowercase.
 
-    Character character = new Character('a'); // create a Character object for the character a.
+    Character character = new Character('a'); // create a Character object for the character 'a'.
 
 following methods:
     • Character(value: char)
@@ -15,7 +15,7 @@ following methods:
     • equals(anotherCharacter: Character): boolean
     • isDigit(ch: char): boolean
     • isLetter(ch: char): boolean
-    • isletterOrDigit(ch: char): boolean
+    • isLetterOrDigit(ch: char): boolean
     • isLowerCase(ch: char): boolean
     • isUpperCae(ch: char): boolean
     • toLowerCase(ch: char): char
