7d85523f41ef9b6a768b8c8969ea82b72f8b0278
Aman <foraman202514@gmail.com>
2026-05-09 23:58:40 +0530

    Added this Keyword Program

---
+++ 10_ThinkingInObjects/src/_02TheScopeOfVariables/_02thisReference.java
@@ -0,0 +1,24 @@
+package _02TheScopeOfVariables;
+
+/*
+this keyword refers to the object itself. It can also be used inside the constructor to invoke another constructor of the same class
+*/
+
+class circle{
+    private double radius;
+
+    public circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return this.radius * this.radius * Math.PI;
+    }
+}
+public class _02thisReference {
+    public static void main(String[] args) {
+        circle c01 = new circle(5);
+
+        System.out.println(c01.getArea());
+    }
+}
