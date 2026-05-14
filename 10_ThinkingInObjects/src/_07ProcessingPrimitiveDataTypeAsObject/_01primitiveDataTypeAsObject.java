b341f7ab880c57e9c477e8b986b8ab215021de4c
Aman <foraman202514@gmail.com>
2026-05-14 23:42:18 +0530

    Added Processing Of Primitive Data Types As Objects Program

---
+++ 10_ThinkingInObjects/src/_07ProcessingPrimitiveDataTypeAsObject/_01primitiveDataTypeAsObject.java
@@ -0,0 +1,7 @@
+package _07ProcessingPrimitiveDataTypeAsObject;
+/*
+In, Java primitive data types(int, double, char, boolean, etc) are not objects- but java provides a wrapper classes that allows primitive data types to be terated as objects.
+*/
+public class _01primitiveDataTypeAsObject {
+    
+}
