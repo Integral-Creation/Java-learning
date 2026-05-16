6152a22429c2c0f90dab5a73168b1dd87b71a50c
Aman <foraman202514@gmail.com>
2026-05-16 23:55:22 +0530

    Added Inheritance Program

---
+++ 11_ InheritanceAndPolymorphism/src/_01Intro/_01Inheritance.java
@@ -0,0 +1,34 @@
+package _01Intro;
+
+/*
+Inheritance in java is core OOPs concept that allows a class to acquire properties and behaviors from another class. It helps in creating a new class from existing class.
+It is an important and powerful features for reusing same code.
+*/
+
+class Animal{ // parents
+    void sound(){
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal{ // child
+    void sound(){
+        System.out.println("Dog barks");
+    }
+}
+class Neko extends Animal{ // child
+    void sound(){
+        System.out.println("Mew.. Mew..");
+    }
+}
+public class _01Inheritance {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.sound(); // Animal makes a sound
+
+        Dog d = new Dog();
+        d.sound(); // Dog barks
+
+        Neko n = new Neko();
+        n.sound(); // Mew.. Mew..
+    }  
+}
