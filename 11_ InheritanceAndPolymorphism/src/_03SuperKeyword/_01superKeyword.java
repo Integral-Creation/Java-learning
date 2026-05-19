f0cf05cf3fc029171b72a8ac5f3c9184c3d6faf9
Nikola-Tesla <foraman202514@gmail.com>
2026-05-19 23:54:59 +0530

    Modified Super Keyword

--- 11_ InheritanceAndPolymorphism/src/_03SuperKeyword/_01superKeyword.java
+++ 11_ InheritanceAndPolymorphism/src/_03SuperKeyword/_01superKeyword.java
@@ -1,13 +1,13 @@
 package _03SuperKeyword;
 /* 
 Super Keyword in java is use to refer to the immediate parent class objects in a inheritance hierarchy.
-It allows subclass to explicitly access the parents class data members or method while they were hiddien or overidden.
+It allows subclass to explicitly access the parents class data members or method while they were hidden or overridden.
 It can also be used to access or invoke the constructor of Superclass or parent Class.
 
     Syntax:
         super() or super(parameter)
 
-    In prevoius Geometric Program:
+    In previous Geometric Program:
             public Circle(String color, boolean filled, double radius){
                     this.radius = radius;
                     setColor(color);
