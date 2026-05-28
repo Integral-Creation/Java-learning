009c11a5c2ca74e5d96f45f9a6f169b08337ae67
Nikola-Tesla <foraman202514@gmail.com>
2026-05-29 00:12:11 +0530

    Modified instanceof Operator Program

--- 11_ InheritanceAndPolymorphism/src/_08Instanceof/_01instanceOfOperator.java
+++ 11_ InheritanceAndPolymorphism/src/_08Instanceof/_01instanceOfOperator.java
@@ -1,9 +1,36 @@
 /*
-
+instanceof operator
+    It is used to check weather a objects belongs to a particular class or interface.
+    Syntax:
+        object instanceof ClassName
 */
+class Animal{
+    void sound(){
+        System.out.println("Animal makes Sound");
+    }
+}
 
+class Neko extends Animal{
+    void sound(){
+        System.out.println("Mew.. Mew");
+    }
+}
 public class _01instanceOfOperator{
     public static void main(String[] args){
+        Neko n = new Neko();
 
+        System.out.println(n instanceof Neko); // true
+        System.out.println(n instanceof Animal); // true
+        System.out.println(n instanceof Object); // true 
+
+        Animal a = new Neko();
+
+        System.out.println(a instanceof Neko); // true
+        System.out.println(a instanceof Animal); // true
+
+
+        Animal animal = new Animal();
+
+        System.out.println(animal instanceof Neko); // False -> here object is Animal not Neko
     }
 }
