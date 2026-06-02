3d9f8f9c6ac0f8df26c1a4ebd80b30c9734bdbf0
Nikola-Tesla <foraman202514@gmail.com>
2026-06-02 23:56:23 +0530

    Modified Protected Access Modifier Program

--- 11_ InheritanceAndPolymorphism/src/_10AccessModifier/protectedAccessModifier/Main.java
+++ 11_ InheritanceAndPolymorphism/src/_10AccessModifier/protectedAccessModifier/Main.java
@@ -1,5 +1,36 @@
 import p2.GlobalBike;
+/*
++----------------------+
+|    GlobalVehicle     |
++----------------------+
+| # speed : int        |
++----------------------+
 
+           ▲
+           |
+           | extends
+           |
++----------------------+
+|     GlobalBike       |
++----------------------+
+|                      |
++----------------------+
+| + showSpeed() : void |
++----------------------+
+
+           │
+           │ creates/uses
+           ▼
+
++----------------------+
+|        Main          |
++----------------------+
+|                      |
++----------------------+
+| + main(args:String[])|
+|   : void             |
++----------------------+
+*/
 public class Main{
     public static void main(String[] args) {
         GlobalBike bike = new GlobalBike();
