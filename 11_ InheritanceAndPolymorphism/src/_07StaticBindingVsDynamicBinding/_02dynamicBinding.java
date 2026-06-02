e9570c3d11eb6090d230bf59f3a99821b2d36905
Nikola-Tesla <foraman202514@gmail.com>
2026-06-02 20:04:18 +0530

    Modified Dynamic Binding

--- 11_ InheritanceAndPolymorphism/src/_07StaticBindingVsDynamicBinding/_02dynamicBinding.java
+++ 11_ InheritanceAndPolymorphism/src/_07StaticBindingVsDynamicBinding/_02dynamicBinding.java
@@ -3,13 +3,13 @@ Dynamic Binding
     Dynamic binding or late binding or runtime polymorphism means that the methods to be executed is decided at runtime, not at compile time.
 */
 
-class Animal{
+class AnimalClass{
     void sound(){
         System.out.println("Animal makes sound");
     }
 }
 
-class Dog extends Animal{
+class Dog extends AnimalClass{
     @Override void sound(){
         System.out.println("Dog barks");
     }
@@ -17,7 +17,7 @@ class Dog extends Animal{
 
 public class _02dynamicBinding{
     public static void main(String[] args){
-        Animal a = new Dog();
+        AnimalClass a = new Dog();
 
         a.sound();
     }
