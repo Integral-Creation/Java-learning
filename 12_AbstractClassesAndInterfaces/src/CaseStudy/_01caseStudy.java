2bd4e800765d5f850d3b3b7ea614f489f2dcb8b7
Aman <foraman202514@gmail.com>
2026-06-07 23:00:05 +0530

    Added CaseStudy Program

---
+++ 12_AbstractClassesAndInterfaces/src/CaseStudy/_01caseStudy.java
@@ -0,0 +1,37 @@
+package CaseStudy;
+
+import java.util.Scanner;
+
+interface Camera{
+    default void boat(){
+        System.out.println("Camera booting...");
+    }
+}
+
+interface Microphone{
+    default void boat(){
+        System.out.println("Microphone booting...");
+    }
+}
+
+class SmartDisplay implements Camera, Microphone{
+    @Override
+    public void boat(){
+        Camera.super.boat();
+        Microphone.super.boat();
+
+        System.out.println("System ready.");
+    }
+}
+public class _01caseStudy {
+    public static void main(String[] args) {
+        Scanner scanner = new  Scanner(System.in);
+        int n = scanner.nextInt();
+
+        SmartDisplay s = new SmartDisplay();
+        for(int i = 0; i < n; i++){
+            s.boat();
+        }
+        scanner.close();
+    }
+}
