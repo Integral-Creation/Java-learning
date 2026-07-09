a71aeb5d69313092105fe63ca9ab3da10fbb4b61
Aman <foraman202514@gmail.com>
2026-07-09 23:18:19 +0530

    Added Intro To GUI Basic

---
+++ 14_GUIBasics/src/_01Intro/_01intro.java
@@ -0,0 +1,14 @@
+/*
+GUI Basic in java
+    GUI(Graphical User Interface) is a visual interface that allows user to interact with a program using the windows, button, textField, menus, and other graphical components instead of typing commands.
+
+    Java provides two main libraries for creating GUI:
+        • AWT(Abstract windows toolkit) - older platform-dependent
+        • Swing - advanced modern, platform-independent, built on AWT.
+
+    Java GUI is a excellent Pedagogical tool for learning object oriented programming.
+*/
+
+public class _01intro {
+    
+}
