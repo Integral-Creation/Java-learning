7644d73d7973c71430f581fcb8a684fcd2abc1ff
Aman <foraman202514@gmail.com>
2026-07-10 23:38:17 +0530

    Added Java GUI API file

---
+++ 14_GUIBasics/src/_01Intro/_02JavaGUIAPI.java
@@ -0,0 +1,24 @@
+/*
+    Java GUI API
+        The GUI API contains classes that can be classified into three group: Component, classes, Container classes, and helper classes.
+        
+    • Java GUI API Hierarchy 
+
+                java.lang.Object
+                        |
+                    Component
+                        |
+                    Container
+                    /      \
+                Window    Panel
+                 |          |
+                Frame      Applet
+
+    • Component Class
+        In java GUI Programming, the Component class is the root class for all the user interface classes. It belong to the java.awt Package.
+        • JComponent is the root class for the lightweight swing component. Both Component and JComponent are abstract class
+*/
+
+public class _02JavaGUIAPI {
+    
+}
