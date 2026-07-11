0808c33a25a42af90bd076bb59077842c6c3ba00
Aman <foraman202514@gmail.com>
2026-07-11 23:41:12 +0530

    Modified Java GUI API

--- 14_GUIBasics/src/_01Intro/_02JavaGUIAPI.java
+++ 14_GUIBasics/src/_01Intro/_02JavaGUIAPI.java
@@ -13,10 +13,30 @@
                 Window    Panel
                  |          |
                 Frame      Applet
-
+    • Swing class extends AWT class
+        Object
+        |
+        Component
+        |
+        Container
+        |
+        JComponent
+        |
+        --------------------------------------
+        |      |      |      |       |       |
+        JButton JLabel JTextField JTextArea JCheckBox ...
+    
     • Component Class
         In java GUI Programming, the Component class is the root class for all the user interface classes. It belong to the java.awt Package.
         • JComponent is the root class for the lightweight swing component. Both Component and JComponent are abstract class
+    • Container class
+        An instance of Container can holds instance of Component. It is used to hold and organize the other GUI component, such as button, labels, textfield, checkbox and the other container.
+
+    • GUI Helper class
+        GUI helper class are the utility classes to provided by java to simplify the creation and management of User interface.
+        some commonly used helper class are Graphics, Color, Font, FontMetrics, Dimension, and LayoutManager etc.
+    
+    
 */
 
 public class _02JavaGUIAPI {
