62c97de587dcb52790034e1806a5abcff3eb7521
Aman <foraman202514@gmail.com>
2026-07-12 21:19:15 +0530

    Added More Component To Frame Program

---
+++ 14_GUIBasics/src/_02Frame/_03addingMoreComponentToFrame.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class _03addingMoreComponentToFrame {
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("MyFrame");
+        frame.setSize(400,300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // create an ImageIcon
+        ImageIcon image = new ImageIcon("14_GUIBasics/src/_02Frame/chess-knight-solid.png");
+        frame.setIconImage(image.getImage());
+        
+        // frame.getContentPane().setBackground(Color.DARK_GRAY); // change color of background
+        frame.getContentPane().setBackground(new Color(36,36,35)); // change color of background
+
+        frame.setVisible(true);
+    }
+}
