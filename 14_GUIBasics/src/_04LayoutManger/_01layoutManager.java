14f51ad63fc11d1200cf2b4507c7c103845e43f9
Aman <foraman202514@gmail.com>
2026-07-14 20:41:19 +0530

    Added Introduction To Layout

---
+++ 14_GUIBasics/src/_04LayoutManger/_01layoutManager.java
@@ -0,0 +1,29 @@
+/*
+layout Manager 
+    In java Swing, layout manger is the object that automatically determine the size and position of the component(such as button, textLabel, and textField etc.) inside a container like JFrame, JPanel.
+
+        • Automatically arranges component 
+        • makes the GUI responsive when the window is resize.
+        • reduces the manual coding
+        • makes the interface easier to maintain
+        • provides platform independent layout
+
+    Layout manager are set in a container using the setLayout(aLayoutManager) method.
+        syntax:
+            LayoutManager layoutManager = new XLayout();
+    
+    Common layout managers:
+    | Layout Manager  | Description                                                                    |
+    | --------------- | ------------------------------------------------------------------------------ |
+    | •FlowLayout`    | Places components left to right like words in a sentence.                      |
+    | •BorderLayout`  | Divides the container into five regions: North, South, East, West, and Center. |
+    | •GridLayout`    | Arranges components in a grid of equal-sized rows and columns.                 |
+    | `BoxLayout`     | Arranges components in a single row or column.                                 |
+    | `GridBagLayout` | Flexible grid layout with different component sizes.                           |
+    | `CardLayout`    | Displays one component at a time, like flipping cards.                         |
+
+*/
+
+public class _01layoutManager {
+    
+}
