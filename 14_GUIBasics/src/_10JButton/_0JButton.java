d1da020c066dd51a699841c69ed2b5af1b4930a3
Aman <foraman202514@gmail.com>
2026-07-17 22:39:07 +0530

    Added JButton Intro

---
+++ 14_GUIBasics/src/_10JButton/_0JButton.java
@@ -0,0 +1,46 @@
+/*
+    JButton
+        JButton is swing component it is used to create clickable button in GUI. It is one of the most commonly used for performing action when user clicks it.
+    
+    • Declaration
+        import javax.swing.JButton;
+
+    The common feature of these are defined in javax.swing.AbstractButton;
++-----------------------------------------------------------------------+
+|               javax.swing.AbstractButton                              |
++-----------------------------------------------------------------------+
+| - actionCommand : String                                              | 
+| - text : String                                                       |
+| - icon : javax.swing.Icon                                             |
+| - pressedIcon : javax.swing.Icon                                      |
+| - rolloverIcon : javax.swing.Icon                                     |
+| - mnemonic : int                                                      |
+| - horizontalAlignment : int                                           |
+| - horizontalTextPosition : int                                        |
+| - verticalAlignment : int                                             |
+| - verticalTextPosition : int                                          |
+| - borderPainted : boolean                                             |
+| - iconTextGap : int                                                   |
+| - selected : boolean                                                  |
++-----------------------------------------------------------------------+
+
+    JButton inherits AbstractButton and provides several constructor to create buttons
+
+                    javax.swing.AbstractButton
+                                ▲
+                                │
+        +------------------------------------------------------+
+        |                javax.swing.JButton                   |
+        +------------------------------------------------------+
+        |                                                      |
+        +------------------------------------------------------+
+        | + JButton()                                          |
+        | + JButton(icon: javax.swing.Icon)                    |
+        | + JButton(text: String)                              |
+        | + JButton(text: String, icon: javax.swing.Icon)      |
+        +------------------------------------------------------+
+*/
+
+public class _0JButton {
+    
+}
