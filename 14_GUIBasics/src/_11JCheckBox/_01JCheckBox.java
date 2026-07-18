dcc8b42d282c05e2d856f39a5d1c246fc60b2f9f
Aman <foraman202514@gmail.com>
2026-07-18 20:45:40 +0530

    Added JCheckBox Intro

---
+++ 14_GUIBasics/src/_11JCheckBox/_01JCheckBox.java
@@ -0,0 +1,27 @@
+/*
+    JCheckBox 
+        A JCheckBox is a swing component allows the user to select or deselect an options.
+        Unlike the RadioButton, multiple checkboxes can be selected at the same time.
+    
+    • Class Hierarchy
+             javax.swing.AbstractButton
+                       ▲
+                       │
+             javax.swing.JToggleButton
+                       ▲
+                       │
++-----------------------------------------------------------+
+|               javax.swing.JCheckBox                       |
++-----------------------------------------------------------+
+| + JCheckBox()                                             |
+| + JCheckBox(text: String)                                 |
+| + JCheckBox(text: String, selected: boolean)              |
+| + JCheckBox(icon: Icon)                                   |
+| + JCheckBox(text: String, icon: Icon)                     |
+| + JCheckBox(text: String, icon: Icon, selected: boolean)  |
++-----------------------------------------------------------+
+*/
+
+public class _01JCheckBox {
+    
+}
